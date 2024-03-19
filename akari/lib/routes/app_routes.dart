f82c4be0b6b6d9ffb26415d7a5d8eadef90f3372
SalomonMetre <kulosmetros088@gmail.com>
2024-03-19 18:20:42 +0100

    Edit routes

--- akari/lib/routes/app_routes.dart
+++ akari/lib/routes/app_routes.dart
@@ -1,5 +1,7 @@
+import 'package:akari/constants.dart/page_builders.dart';
+import 'package:akari/constants.dart/page_names.dart';
 import 'package:flutter/material.dart';
 
 final appRoutes = <String, WidgetBuilder>{
-  
+  PageNames.home : homeBuilder,
 };
