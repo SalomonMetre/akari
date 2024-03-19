cb09341f208b574736b2ef8f2fe604cf00542542
SalomonMetre <kulosmetros088@gmail.com>
2024-03-19 18:20:19 +0100

    Edit home page

--- akari/lib/screens/home_page.dart
+++ akari/lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:akari/constants.dart/image_names.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,9 +8,50 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            SizedBox(
+              child: ClipOval(
+              child: Image.asset(
+                ImageNames.logoName,
+              ),
+            ),
+            ),
+            LinearProgressIndicator(
+              color: Colors.black,
+              value: controller.value,
+              semanticsLabel: 'Linear progress indicator',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
