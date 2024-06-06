b4003bb3ac80099c7f114d50da16ac4dd993d898
Salomon Metre <kulosmetros088@gmail.com>
2024-06-06 03:01:50 +0200

    functions for updating the cells of game grid

---
+++ akari/lib/functions/cell_functions.dart
@@ -0,0 +1,42 @@
+import 'package:akari/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// Renders the game cell given the current game grid, the row & col of the clicked cell, and the cell size
+Widget renderCell(
+    {required List gridProposition,
+    required int row,
+    required int col,
+    required double cellSize}) {
+  int cellValue = gridProposition[row][col];
+  return switch (cellValue) {
+    -1 => Container(
+        color: Colors.black,
+      ),
+    0 => Container(
+        color: Colors.white,
+      ),
+    10 => const Icon(
+        Icons.light,
+        color: AppColors.yellow,
+      ),
+    20 => Container(
+        color: Colors.yellow,
+      ),
+    _ => Container(
+        width: cellSize,
+        height: cellSize,
+        color: AppColors.black,
+        child: Center(
+          child: Text(
+            "${gridProposition[row][col]}",
+            style: GoogleFonts.cherrySwash(
+              color: AppColors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      )
+  };
+}
