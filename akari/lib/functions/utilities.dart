c77b9dcaf14575a27bb4b74c75681c314186637a
Salomon Metre <kulosmetros088@gmail.com>
2024-06-06 02:59:10 +0200

    utility functions for pop ups, dialogs, etc.

--- akari/lib/functions/utilities.dart
+++ akari/lib/functions/utilities.dart
@@ -1,8 +1,9 @@
+import 'package:akari/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void appExit(){
+void appExit() {
   SystemNavigator.pop();
 }
 
@@ -16,18 +17,26 @@ Future<bool?> showYesNoDialog(BuildContext context,
         content: Text(questionText),
         actions: <Widget>[
           ElevatedButton(
-            style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.red)),
+            style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.red)),
             onPressed: () {
               Navigator.of(context).pop(false);
             },
-            child: Text('No', style: GoogleFonts.cabin(color: Colors.white),),
+            child: Text(
+              'No',
+              style: GoogleFonts.cabin(color: Colors.white),
+            ),
           ),
           ElevatedButton(
-            style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.teal)),
+            style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.teal)),
             onPressed: () {
               Navigator.of(context).pop(true);
             },
-            child: Text('Yes', style: GoogleFonts.cabin(color: Colors.white),),
+            child: Text(
+              'Yes',
+              style: GoogleFonts.cabin(color: Colors.white),
+            ),
           ),
         ],
       );
@@ -36,23 +45,122 @@ Future<bool?> showYesNoDialog(BuildContext context,
 }
 
 Future<bool?> showQuitDialog(BuildContext context) async {
-    return await showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Quit Game?'),
-          content: const Text('Are you sure you want to quit the game?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text('Yes'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text('No'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  return await showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Quit Game?'),
+        content: const Text('Are you sure you want to quit the game?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Yes'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+void showLevelEndDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: const Text(
+            textAlign: TextAlign.center,
+            'Congratulations!',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+        backgroundColor: AppColors.blue1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        content: const Text(
+          textAlign: TextAlign.right,
+          'You have solved the puzzle!',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+        actions: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(AppColors.blue3),
+                      minimumSize: WidgetStateProperty.all(
+                          const Size(double.infinity, 50)),
+                    ),
+                    onPressed: () {
+                      // TODO: Implement save my score functionality
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Save Score',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Colors.green),
+                      minimumSize: WidgetStateProperty.all(
+                          const Size(double.infinity, 50)),
+                    ),
+                    onPressed: () {
+                      // TODO: Implement next level functionality
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Next Level',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Colors.red),
+                      minimumSize: WidgetStateProperty.all(
+                          const Size(double.infinity, 50)),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Close',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    },
+  );
+}
